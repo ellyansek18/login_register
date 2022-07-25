1ae973e862dc35bb5a2e6fe98bd50f7fb93fc56e
ellyansek18 <ellyansek18@gmail.com>
2022-07-25 11:12:56 +0900

    login_register

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    require 'config.php';
+    if(!empty($_SESSION["id"])){
+    $id = $_SESSION["id"];
+    $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE id = $id");
+    $row = mysqli_fetch_assoc($result);
+    }
+    else{
+    header("Location: login.php");
+    }
+?>
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Index</title>
+  </head>
+
+  <body>
+    <h1>Welcome, <?php echo $row["name"]; ?></h1> <br>
+    <h2>Di SaCode School</h2>
+    <a href="logout.php">Logout</a>
+  </body>
+
+</html>
